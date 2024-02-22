9ad47174532fd3306825473f8d0f4a0cb298ae62
EyesEagle9 <110094556+EyesEagle9@users.noreply.github.com>
2024-02-22 10:20:42 +0100

    test-deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json()); // Body parser integrated with Express
+app.use(express.json()); 
 
 const db = mysql.createPool({
   port: process.env.DB_PORT,
@@ -18,36 +18,30 @@ const db = mysql.createPool({
   database: process.env.DB_DATABASE,
 });
 
-// Vérification de la connexion à la base de données au démarrage du serveur
 db.getConnection((error, connection) => {
   if (error) {
     console.error("Failed to connect to database:", error);
-    process.exit(1); // Exit the process if unable to connect to database
   }
   console.log("Connection to database successful");
-  connection.release(); // Release the connection
+  connection.release(); 
 });
 
-// Route pour la page d'accueil
 app.get("/", (req, res) => {
   res.send("Hello Everyone");
 });
 
-// Route pour gérer les messages de contact
-app.post("/contact", (req, res) => {
+// Contact
+app.post("/contact", (req, res, next) => {
   let adress = req.body.adress;
   let message = req.body.message;
   
-  // Requête SQL pour insérer les données de contact dans la base de données
-  let qr = `INSERT INTO user (adress, message) VALUES (?, ?)`;
+  let qr = `INSERT INTO contact (adress, message) VALUES (?, ?)`;
   
-  // Exécution de la requête SQL
   db.query(qr, [adress, message], (error, results) => {
     if (error) {
       console.error("SQL query execution error:", error);
-      return res.status(500).send({ message: "Internal server error" });
+      return next(error); 
     }
-    // Vérification si des lignes ont été affectées par la requête
     if (results.affectedRows > 0) {
       return res.status(200).send({ message: "Message sent successfully" });
     } else {
@@ -56,7 +50,6 @@ app.post("/contact", (req, res) => {
   });
 });
 
-// Démarrage du serveur
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
